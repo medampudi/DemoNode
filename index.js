4e5075d787e44b268a5f710a52a5aafd53534e84
Rajesh Medampudi <medampudi@gmail.com>
2012-09-26 03:37:11 +0530

    A server that accepts files and displays them after it has been uploaded.

--- index.js
+++ index.js
@@ -9,5 +9,23 @@ var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle); 
+ 
+ 
+ 
+ 
+/* 
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
 
 server.start(router.route, handle);
+
+*/
--- router.js
+++ router.js
@@ -1,19 +1,16 @@
 /**
  * New node file
  */
-function route(handle, pathname,response,postData) {
-
+function route(handle, pathname, response, request) {
   console.log("About to route a request for " + pathname);
-  if(typeof handle[pathname]==='function'){
-  	handle[pathname](response,postData);
-  }else{
-  	console.log("No request Handler found for "+ pathname);
-  	response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("404 Not Found");
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response, request);
+  } else {
+    console.log("No request handler found for " + pathname);
+    response.writeHead(404, {"Content-Type": "text/html"});
+    response.write("404 Not found");
     response.end();
-  	
   }
-
 }
 
 exports.route = route;
