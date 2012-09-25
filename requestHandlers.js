6aa387702d03a51addbf86e938321a66fb33ca77
Rajesh Medampudi <medampudi@gmail.com>
2012-09-25 17:40:06 +0530

    example of request handlers with non-blocking operations

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,21 +1,38 @@
 /**
  * New node file
  */
-function start() {
+ 
+ var exec = require("child_process").exec;
+ 
+function start(response) {
   console.log("Request handler 'start' was called.");
+  var content = "empty";
   
-  function sleep(milliseconds){
-  	var startTime = new Date().getTime();
-  	while (new Date().getTime() < startTime + milliseconds);
-   }
-	sleep(10000); 
+  exec("find /",
+  		{timeout: 10000, maxBuffer: 20000*1024},
+  		function(error,stdout,stderr){
+  	response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end(); 
   
-  return "Hello Start";
+  });
+  
+  return content;
+  
+//  function sleep(milliseconds){
+//  	var startTime = new Date().getTime();
+// 	while (new Date().getTime() < startTime + milliseconds);
+//   }
+//	sleep(10000); 
+// 
+//  return "Hello Start";
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+   response.writeHead(200, {"Content-Type": "text/plain"});
+   response.write("Hello Upload");
+   response.end();
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -1,14 +1,17 @@
 /**
  * New node file
  */
-function route(handle, pathname) {
+function route(handle, pathname,response) {
 
   console.log("About to route a request for " + pathname);
   if(typeof handle[pathname]==='function'){
-  	return handle[pathname]();
+  	handle[pathname](response);
   }else{
   	console.log("No request Handler found for "+ pathname);
-  	return "404 Not Found";
+  	response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("404 Not Found");
+    response.end();
+  	
   }
 
 }
