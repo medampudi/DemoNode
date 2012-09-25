c1fbd0bc160dafffa6a0af6d53b203a2f8734bac
Rajesh Medampudi <medampudi@gmail.com>
2012-09-25 17:12:22 +0530

    blocking example from request handlers

---
+++ requestHandlers.js
@@ -0,0 +1,22 @@
+/**
+ * New node file
+ */
+function start() {
+  console.log("Request handler 'start' was called.");
+  
+  function sleep(milliseconds){
+  	var startTime = new Date().getTime();
+  	while (new Date().getTime() < startTime + milliseconds);
+   }
+	sleep(10000); 
+  
+  return "Hello Start";
+}
+
+function upload() {
+  console.log("Request handler 'upload' was called.");
+  return "Hello Upload";
+}
+
+exports.start = start;
+exports.upload = upload;
--- router.js
+++ router.js
@@ -1,7 +1,16 @@
 /**
  * New node file
  */
-function route(pathname) {
+function route(handle, pathname) {
+
   console.log("About to route a request for " + pathname);
+  if(typeof handle[pathname]==='function'){
+  	return handle[pathname]();
+  }else{
+  	console.log("No request Handler found for "+ pathname);
+  	return "404 Not Found";
+  }
+
 }
+
 exports.route = route;
